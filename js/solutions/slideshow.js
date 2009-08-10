64eaa04b819a857b66ff73c425266fb26ed6341f
Rebecca Murphey <rebecca@rebeccamurphey.com>
2009-08-10 18:36:53 -0400

    moving index js to individual files, renaming advanced slideshow file

--- js/solutions/slideshow.js
+++ js/solutions/slideshow.js
@@ -1,99 +1,38 @@
-/* slideshow */
-var slideshow = {
-	
-	config : {
-		autoAdvance : true
-	},
-	
-	init : function(config) {
-		
-		var config = config || {};
-		$.extend(slideshow.config, config);
-		
-		slideshow.$hero = $('#hero').prependTo('#main')
-			.addClass('js-slideshow');
-		slideshow.$items = slideshow.$hero.find('li').hide();
-		
-		slideshow.$items.each(function(i) {
-			$(this).data('number', i + 1);
-		});
-		
-		slideshow.totalItems = slideshow.$items.length;
-		
-		slideshow.createNav();
-		
-		slideshow.show(slideshow.$items.filter(':first')); 
-		
-		if (slideshow.config.autoAdvance) {
-			slideshow.interval = 
-				setInterval(slideshow.showNext, 2000);
-		}
-	},
-	
-	createNav : function() {
-		slideshow.$next = $('<p>Next</p>')
-			.insertAfter(slideshow.$hero)
-			.click(function() {
-				clearInterval(slideshow.interval);
-				slideshow.showNext();
-			});
-		slideshow.$counter = $('<p></p>')
-			.insertAfter(slideshow.$hero);
-		slideshow.$prev = $('<p>Prev</p>')
-			.insertAfter(slideshow.$hero)
-			.click(function() {
-				clearInterval(slideshow.interval);
-				slideshow.showPrev();
-			});
-	},
-	
-	showNext : function() {
-		slideshow.show(slideshow.getNextItem());
-	},
-	
-	showPrev : function() {
-		slideshow.show(slideshow.getPrevItem());
-	},
-	
-	show : function($item) {
-		slideshow.hide($item.siblings());
-		$item.fadeIn(500);
-		slideshow.updateCounter();
-	},
-	
-	hide : function($items) {
-		$items.hide();
-	},
-	
-	getCurrentItem : function() {
-		return slideshow.$items.filter(':visible');
-	},
-	
-	getNextItem : function() {
-		var $current = slideshow.getCurrentItem();
-		if ($current.next().length) {
-			return $current.next();
-		} else {
-			return slideshow.$items.filter(':first');
-		}
-	},
-	
-	getPrevItem : function() {
-		var $current = slideshow.getCurrentItem();
-		if ($current.prev().length) {
-			return $current.prev();
-		} else {
-			return slideshow.$items.filter(':last');
-		}
-	},
-	
-	updateCounter : function() {
-		var text = slideshow.getCurrentItem().data('number') 
-			+ ' of ' + slideshow.totalItems;
-		slideshow.$counter.text(text);
-	}
-};
-
 $(document).ready(function() {
-	slideshow.init();
+	var $slideshow = $('#hero');
+
+	// move the slideshow up to the top
+	$slideshow.prependTo('#main');
+
+	// set up the slideshow
+	$slideshow.find('li')
+
+		// hide all the list items
+		.hide()
+
+		// bind the click behavior: 
+		// fade out the current item,
+		// then fade in the next item
+		.click(function(){
+
+			// assign the clicked element 
+			// to a variable
+			var $this = $(this);
+
+			// is there a next item? if not, 
+			// use the first item as the next
+			var $next = $this.next();
+
+			if (! $next.length) {
+				$next = $slideshow.find('li:first');
+			}
+
+			// do the fade out, then the fade in
+			$this.fadeOut(500, function() {
+				$next.fadeIn(500);
+			});
+		})
+
+		// show the first item
+		.eq(0).show();	
 });
