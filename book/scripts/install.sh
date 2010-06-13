6f6efcd2c1c4ef33d018c57dcc527b1eade03174
Rebecca Murphey <rebecca@rebeccamurphey.com>
2010-06-13 16:57:02 -0400

    initial work on book as docbook

---
+++ book/scripts/install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Script is from http://github.com/recess/the-book-of-recess/blob/master/scripts/install.sh
+
+# Prerequisites:
+#	Java
+
+if [ ! -d "./libs" ]; then
+	mkdir "libs"
+fi
+cd libs
+
+# Install necessary components for Docbook Publishing
+#	1) Xalan
+#	2) Docbook XSL NS Stylesheets
+
+# Xalan Install
+if [ ! -f xalan.zip ]; then
+	echo "Downloading and extracting Xalan"
+	curl -o xalan.zip http://mirrors.ibiblio.org/pub/mirrors/apache/xml/xalan-j/xalan-j_2_7_1-bin-2jars.zip
+	unzip xalan.zip -d xalan
+fi
+
+# FOP Install
+if [ ! -f fop.zip ]; then
+	echo "Downloading and extracting FOP"
+	curl -o fop.zip http://mirrors.ibiblio.org/pub/mirrors/apache/xmlgraphics/fop/binaries/fop-0.95-bin.zip
+	unzip fop.zip -d fop
+fi
+
+# DocBook NS XSL Stylesheets
+if [ ! -f docbook-xsl.zip ]; then
+	echo "Downloading and extracting DocBook XSL NS"
+	curl -o docbook-xsl.zip http://voxel.dl.sourceforge.net/sourceforge/docbook/docbook-xsl-ns-1.75.0.zip
+	unzip docbook-xsl.zip -d docbook-xsl
+fi
+
+# XSL Syntax Highlight
+if [ ! -f xslthl.zip ]; then
+	echo "Downloading and extracting XSLT Syntax Highlighting"
+	curl -o xslthl.zip http://voxel.dl.sourceforge.net/sourceforge/xslthl/xslthl-2.0.1.zip
+	unzip xslthl.zip -d xslthl
+fi
+
+echo "The DocBook XSL tools have been installed."
+echo "Publish with: ./scripts/publish.sh"
---
+++ book/scripts/publish.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+input=the-book-of-recess.xml
+
+base=`pwd`
+
+# Next is a hack for cross-platform win/linux absolute file for Java
+absolute=`echo $base | sed 's|/\([a-z]\)/|/\1:/|'`
+
+xalan=libs/xalan/xalan-j_2_7_1
+xslthlJar=libs/xslthl/xslthl-2.0.1.jar
+xsl=libs/docbook-xsl/docbook-xsl-ns-1.75.0
+xslChunked=xsl/xhtml-chunked.xsl
+xslAllInOne=xsl/xhtml.xsl
+xslFO=xsl/fo.xsl
+fop=libs/fop/fop-0.95
+outputAllInOne=pub/html/the-book-of-recess.html
+outputChunked=pub/html/index.html
+outputFO=pub/fo/the-book-of-recess.fo
+outputPDF=pub/pdf/the-book-of-recess.pdf
+
+if [ ! -f $xalan/xalan.jar ]; then
+	echo "Xalan not found. Run install script: ./scripts/install.sh"
+	exit
+fi
+
+if [ ! -f $xsl_template ]; then
+	echo "XSL stylesheets not found. Run install script: ./scripts/install.sh"
+	exit
+fi
+
+if [ ! -d "pub" ]; then
+	mkdir pub
+fi
+
+if [ ! -d "pub/html" ]; then
+	mkdir pub/html
+fi
+
+if [ ! -d "pub/fo" ]; then
+	mkdir pub/fo
+fi
+
+if [ ! -d "pub/pdf" ]; then
+	mkdir pub/pdf
+fi
+
+cp -Rf imgs pub/html
+cp style/* pub/html
+cp -Rf $xsl/images pub/html
+
+java \
+	-Djava.endorsed.dirs=$xalan  \
+    -cp "$xalan/xalan.jar;$xalan/xml-apis.jar;$xalan/xercesImpl.jar;$xsl/extensions/xalan27.jar;$xslthlJar" \
+    -Dorg.apache.xerces.xni.parser.XMLParserConfiguration=org.apache.xerces.parsers.XIncludeParserConfiguration \
+	 org.apache.xalan.xslt.Process \
+    -in $input  \
+    -out $outputChunked  \
+    -xsl $xslChunked  \
+	-param keep.relative.image.uris 0 \
+    -param use.extensions 1 \
+    -param highlight.xslthl.config "file://$absolute/libs/xslthl/highlighters/xslthl-config.xml" \
+    -param highlight.source 1
+
+java \
+	-Djava.endorsed.dirs=$xalan  \
+    -cp "$xalan/xalan.jar;$xalan/xml-apis.jar;$xalan/xercesImpl.jar;$xsl/extensions/xalan27.jar;$xslthlJar" \
+    -Dorg.apache.xerces.xni.parser.XMLParserConfiguration=org.apache.xerces.parsers.XIncludeParserConfiguration \
+	org.apache.xalan.xslt.Process \
+    -in $input  \
+    -out $outputAllInOne  \
+    -xsl $xslAllInOne  \
+	-param keep.relative.image.uris 0 \
+    -param use.extensions 1 \
+    -param highlight.xslthl.config "file://$absolute/libs/xslthl/highlighters/xslthl-config.xml" \
+    -param highlight.source 1
+    
+java \
+	-Djava.endorsed.dirs=$xalan  \
+    -cp "$xalan/xalan.jar;$xalan/xml-apis.jar;$xalan/xercesImpl.jar;$xsl/extensions/xalan27.jar;$xslthlJar" \
+    -Dorg.apache.xerces.xni.parser.XMLParserConfiguration=org.apache.xerces.parsers.XIncludeParserConfiguration \
+	org.apache.xalan.xslt.Process \
+    -in $input  \
+    -out $outputFO  \
+    -xsl $xslFO  \
+    -param use.extensions 1 \
+    -param highlight.xslthl.config "file://$absolute/libs/xslthl/highlighters/xslthl-config.xml" \
+    -param highlight.source 1
+
+cp -Rf imgs $fop
+
+cd $fop
+java \
+	-jar "build/fop.jar" \
+	-fo ../../../$outputFO -pdf ../../../$outputPDF
+	
+cd $base
+
+rm -rf $fop/imgs
+
+echo "Published Recess DocBook to $output"
