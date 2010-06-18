21b749206cdc53ce7c61d953b77a030117a45ec7
Peter Aronoff <telemachus@arpinum.org>
2010-06-19 04:52:07 +0800

    Make the installer somewhat less fragile
    1. Change shebang to #!/bin/sh - it's probably a pointer to bash anyhow, but
       this removes any issues about _where_ bash lives.
    2. set -e - exit upon failure early
    3. Check for curl then wget and set an appropriate download command depending on
       what we find. Exit if no downloader is found.
    4. Don't assume that the script is being run as ./script/installer.sh. cd to the
       necessary working directory more explicitly and then go from there.

--- book/scripts/install.sh
+++ book/scripts/install.sh
@@ -1,14 +1,34 @@
-#!/bin/bash
+#!/bin/sh
 
-# Script is from http://github.com/recess/the-book-of-recess/blob/master/scripts/install.sh
+# Script is from 
+# http://github.com/recess/the-book-of-recess/blob/master/scripts/install.sh
+# Now with lots of small changes
 
 # Prerequisites:
 #	Java
 
-if [ ! -d "./scripts/libs" ]; then
-	mkdir "scripts/libs"
+# bail out if things get ugly
+set -e
+
+# make sure we have a download option before continuing
+if  which curl >/dev/null 2>&1; then
+    GET='curl -L -o'
+elif which wget >/dev/null 2>&1; then
+    GET='wget -O'
+else
+    echo "Can't find curl or wget. Get one and try again."
+    exit 1
 fi
-cd scripts/libs
+
+# move to relevant working directory
+cd `dirname $0`
+
+if [ -d libs ]; then
+    cd libs
+else
+    mkdir libs
+    cd libs
+fi
 
 # Install necessary components for Docbook Publishing
 #	1) Xalan
@@ -17,30 +37,32 @@ cd scripts/libs
 # Xalan Install
 if [ ! -f xalan.zip ]; then
 	echo "Downloading and extracting Xalan"
-	curl -o xalan.zip http://mirrors.ibiblio.org/pub/mirrors/apache/xml/xalan-j/xalan-j_2_7_1-bin-2jars.zip
+	$GET xalan.zip http://mirrors.ibiblio.org/pub/mirrors/apache/xml/xalan-j/xalan-j_2_7_1-bin-2jars.zip
 	unzip xalan.zip -d xalan
 fi
 
 # FOP Install
 if [ ! -f fop.zip ]; then
 	echo "Downloading and extracting FOP"
-	curl -o fop.zip http://mirrors.ibiblio.org/pub/mirrors/apache/xmlgraphics/fop/binaries/fop-0.95-bin.zip
+	$GET fop.zip http://mirrors.ibiblio.org/pub/mirrors/apache/xmlgraphics/fop/binaries/fop-0.95-bin.zip
 	unzip fop.zip -d fop
 fi
 
 # DocBook NS XSL Stylesheets
 if [ ! -f docbook-xsl.zip ]; then
 	echo "Downloading and extracting DocBook XSL NS"
-	curl -L -o docbook-xsl.zip http://sourceforge.net/projects/docbook/files/docbook-xsl/1.75.2/docbook-xsl-1.75.2.zip/download
+	$GET docbook-xsl.zip http://sourceforge.net/projects/docbook/files/docbook-xsl/1.75.2/docbook-xsl-1.75.2.zip/download
 	unzip docbook-xsl.zip -d docbook-xsl
 fi
 
 # XSL Syntax Highlight
 if [ ! -f xslthl.zip ]; then
 	echo "Downloading and extracting XSLT Syntax Highlighting"
-	curl -L -o xslthl.zip http://sourceforge.net/projects/xslthl/files/xslthl/2.0.2/xslthl-2.0.2.zip/download
+	$GET xslthl.zip http://sourceforge.net/projects/xslthl/files/xslthl/2.0.2/xslthl-2.0.2.zip/download
 	unzip xslthl.zip -d xslthl
 fi
 
+echo
+echo
 echo "The DocBook XSL tools have been installed."
-echo "Publish with: ./scripts/publish.sh"
+echo "Publish with: ./scripts/publish.sh or ./scripts/publish-pdf.sh"
