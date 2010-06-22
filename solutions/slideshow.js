089fbea213ff0a2b4e3c60c2cf6f5956fa8009e4
Rebecca Murphey <rebecca@rebeccamurphey.com>
2010-06-22 16:32:53 -0400

    better & more interesting slideshow solution

--- solutions/slideshow.js
+++ solutions/slideshow.js
@@ -1,38 +1,81 @@
 $(document).ready(function() {
-	// move the slideshow up to the top
-	var $slideshow = $('#slideshow');
-	$slideshow.prependTo('#main');
-
-	// create the counter element
-	var $counter = $('<div/>')
-		.insertAfter($slideshow);
+	var timeout, manualMode = false,
 	
-	var $items = $slideshow.find('li').hide();
-	var total = $items.length;
+		$slideshow = $('#slideshow').prependTo('#main'),
 	
-	var callback = function() {
-		var $this = $(this);
+		$counter = $('<div/>').insertAfter($slideshow),
 		
-		var num = $this.prevAll().length + 1;
+		$controls = $('<div/>').insertAfter($slideshow),
 		
-		var $next = $this.next().length ?
-			$this.next() : $items.eq(0);
+		$prevBtn = $('<input/>', {
+			type : 'button',
+			value : 'previous'
+		}).appendTo($controls),
+		
+		$nextBtn = $('<input/>', {
+			type : 'button',
+			value : 'next'
+		}).appendTo($controls),
+		
+		$items = $slideshow.find('li').hide(),
+		
+		total = $items.length, 
+		
+		updateCounter = function(num) {
+			$counter.text(num + ' of ' + total);
+		},
+		
+		getNextItem = function($item) {
+			return $item.next().length ? 
+				$item.next() : $items.first();
+		},
+		
+		getPrevItem = function($item) {
+			return $item.prev().length ?
+				$item.prev() : $items.last();
+		},
+		
+		showItem = function($currentItem, $itemToShow) {
+			var $itemToShow = 
+				$itemToShow || getNextItem($currentItem);
 			
-		// update the counter
-		$counter.text(num + ' of ' + total);
-			
-		// set the timeout for showing
-		// the next item in 5 seconds
-		setTimeout(function() {
-			// fade out the current element
-			$this.fadeOut(500, function() {
-				// show the next element,
-				// run this same callback
-				// when done
-				$next.fadeIn(500, callback);
+			$currentItem.fadeOut(500, function() {
+				$itemToShow.fadeIn(500, fadeCallback);
 			});
-		}, 5000);
-	};
+		},
+		
+		fadeCallback = function() {
+			if (manualMode) { return; }
+
+			var $this = $(this),
+				$next = getNextItem($this),
+				num = $this.prevAll().length + 1;
+			
+			// update the counter
+			updateCounter(num);
+						
+			// set the timeout for showing
+			// the next item in 5 seconds
+			timeout = setTimeout(function() {
+				showItem($this, $next);
+			}, 1000);
+		},
+		
+		handleBtnClick = function(e) {
+			clearTimeout(timeout);
+
+			manualMode = true;
+			
+			var $currentItem = $items.filter(':visible'),
+				$itemToShow = e.data.prev ? 
+					getPrevItem($currentItem) :
+					getNextItem($currentItem);
+					
+			showItem($currentItem, $itemToShow);
+		};
+		
+	$prevBtn.bind('click', handleBtnClick, { prev : true });
+	$nextBtn.bind('click', handleBtnClick, { next : true });
 	
-	$items.eq(0).fadeIn(500, callback);
+	$items.eq(0).fadeIn(500, fadeCallback);
 });
